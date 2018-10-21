71ee7467bcbe7618555e08a5c261cbf9fffdfe10
ACJMR <chosking@stuy.edu>
2018-10-21 19:01:24 -0400

    changing size to 100

--- sieve.c
+++ sieve.c
@@ -3,7 +3,7 @@
 #include <math.h>
 
 int sieve(int n){
-  int size = n*log(n)*1.15;
+  int size = 100;
   int ray[size];
   for(int n = 0; n<size; n++){
       ray[n] = 0;
@@ -12,11 +12,14 @@ int sieve(int n){
   int focus = 2;
   int i = 2;
   while(count<n){
+    //printf("focus %d\n",focus);
+    //printf("ray[focus] %d\n",ray[focus]);
     if (!ray[focus]){
       while(focus*i<size){
 	       ray[focus*i] = 1;
+         //printf("mult %d\n",focus*i);
 	       i++;
-      }
+       }
       count++;
       i = 2;
     }
